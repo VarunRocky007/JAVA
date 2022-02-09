e481203d057e2b7d4dad5eb2ac226dfbb54c95b9
Varun <varsho2002@gmail.com>
2022-02-09 21:03:50 +0530

    Iteration updated

--- src/Iteration/AssignmentThree.java
+++ src/Iteration/AssignmentThree.java
@@ -5,12 +5,9 @@ public class AssignmentThree {
         char dollar = '$';
         for(int i=1;i<=4;i++){
             for(int j=1;j<=4;j++){
-                if(j==1 || j==4){
+                if(i==1 || i==4 || j==1 || j==4){
                     System.out.print(dollar+" ");
-                    continue;
-                }
-                if(i==1 || i==4){
-                    System.out.print(dollar+" ");
+                    
                 }
                 else {
                     System.out.print(" "+" ");
